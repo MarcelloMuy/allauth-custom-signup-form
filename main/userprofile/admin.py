3706809546498cee316063b4a7ab12be190f7b84
MarcelloMuy <marcellomuylaert@icloud.com>
2022-09-28 05:36:21 +0000

    Create UserProfile and Profession models

--- main/userprofile/admin.py
+++ main/userprofile/admin.py
@@ -1,3 +1,23 @@
+'Imported'
 from django.contrib import admin
+from .models import UserProfile, Profession
 
-# Register your models here.
+
+class ProfessionAdmin(admin.ModelAdmin):
+    """Admin Profession"""
+    list_display = (
+        'name',
+    )
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin UserProfile"""
+    list_display = (
+        'user',
+        'profession',
+        'experience',
+    )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Profession, ProfessionAdmin)
--- main/userprofile/models.py
+++ main/userprofile/models.py
@@ -1,3 +1,28 @@
+'Imported'
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Profession(models.Model):
+    """A model for user role options"""
+
+    name = models.CharField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    """A user profile model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profession = models.ForeignKey(
+        Profession, null=True, on_delete=models.SET_NULL
+        )
+    experience = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+        )
+
+    def __str__(self):
+        return self.user.username
